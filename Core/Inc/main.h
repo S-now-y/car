9806b060fe3839bd409865542b47c4f53226180c
S-now-y <3074031x@student.gla.ac.uk>
2025-02-16 20:49:20 +0800

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define motor3a_Pin GPIO_PIN_5
+#define motor3a_GPIO_Port GPIOE
+#define motor3b_Pin GPIO_PIN_6
+#define motor3b_GPIO_Port GPIOE
+#define motor1a_Pin GPIO_PIN_9
+#define motor1a_GPIO_Port GPIOE
+#define LED1_Pin GPIO_PIN_10
+#define LED1_GPIO_Port GPIOE
+#define motor1b_Pin GPIO_PIN_11
+#define motor1b_GPIO_Port GPIOE
+#define motor2a_Pin GPIO_PIN_13
+#define motor2a_GPIO_Port GPIOE
+#define motor2b_Pin GPIO_PIN_14
+#define motor2b_GPIO_Port GPIOE
+#define motor4a_Pin GPIO_PIN_8
+#define motor4a_GPIO_Port GPIOB
+#define motor4b_Pin GPIO_PIN_9
+#define motor4b_GPIO_Port GPIOB
+#define key1_Pin GPIO_PIN_0
+#define key1_GPIO_Port GPIOE
+#define key2_Pin GPIO_PIN_1
+#define key2_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
